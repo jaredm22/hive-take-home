c87f0bc6181c1387aa86ab291fb2b9b0f77b086e
Jared Min <jkymin2@gmail.com>
2022-02-18 15:23:22 -0500

    Set up examples

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,25 @@ import DropdownMenu from "./components/DropdownMenu";
 export default function Home() {
   return (
     <div className="container">
+        
         <div className="examples-container">
             <h1>Dropdown Menu</h1>
+            <div style={{display: "flex", flexDirection:"row",  justifyContent: 'space-evenly', margin: "1rem 0", width: 1024}}>
+                <h3>Single Select</h3>
+                <DropdownMenu 
+                    multipleSelect={false} 
+                    label={"Age"}
+                    items={["None", "Twenty", "Twenty one", "Twenty one and a half"]}
+                />
 
-            <h3>Single Select</h3>
-            <DropdownMenu 
-                multipleSelect={false} 
-                label={"Age"}
-                items={["None", "Twenty", "Twenty one", "Twenty one and a half"]}
-            />
-
-            <h3>Multiple Select</h3>
-            <DropdownMenu 
-                multipleSelect={true} 
-                label={"Tag"} 
-                items={["Oliver Hansen", "Van Henry", "April Tucker", "Ralph Hubbard"]}
-            />           
+                <h3>Multiple Select</h3>
+                <DropdownMenu 
+                    multipleSelect={true} 
+                    label={"Tag"} 
+                    items={["Oliver Hansen", "Van Henry", "April Tucker", "Ralph Hubbard"]}
+                />     
+            </div>
+                  
         </div>
         
     </div>
